acd17ccae24e04ddf2179d55dff779270bbb04a1
Ajand Mardalizad <aseedgeek@gmail.com>
2023-11-27 20:43:59 +0400

    minor bug fix

--- ui/lib/ipfsResolvers.js
+++ ui/lib/ipfsResolvers.js
@@ -1,13 +1,13 @@
 export const ipfsUrlToNormal = (url) => {
   if (url?.includes("ipfs://")) {
     const cid = url.replace("ipfs://", "");
-    return `https://cloudflare-ipfs.com/ipfs/${cid}`;
+    return `https://api.universalprofile.cloud/ipfs/${cid}`;
   }
   return url;
 };
 
 export const ipfsUrlResolver = (cid) => {
-  return `https://cloudflare-ipfs.com/ipfs/${cid}`;
+  return `https://api.universalprofile.cloud/ipfs/${cid}`;
 };
 
 export const ipfsAddressResolver = (cid) => {
